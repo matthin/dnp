03324c430cb72dfb5e7f53a41e23b26bf59d6567
Justin Harrison <justin@matthin.com>
2015-07-01 15:35:15 -0400

    Finalize listener public methods spec

--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 require "dnp/listener"
 
+require "dnp/server_client"
+
 describe(Dnp::Listener) do
   context("binds to a host") do
     # Bind to any available port
@@ -21,6 +23,30 @@ describe(Dnp::Listener) do
         listener.instance_variable_get("@socket").addr[3]
       ).to(eq(host))
     end
+  end
+
+  it("binds to a port") do
+    listener = Dnp::Listener.new(PORT)
+    expect(
+      listener.instance_variable_get("@socket").addr[1]
+    ).to(be_a(Integer))
+  end
+
+  it("accepts a new client") do
+    listener = Dnp::Listener.new(PORT)
+    server_client = double(Dnp::ServerClient)
+
+
+    accept_thread = Thread.new do
+      expect(
+        listener.accept
+      ).to(eq(server_client))
+    end
+
+    sleep(0.01)
+    listener.instance_variable_set("@clients", [server_client])
+
+    accept_thread.join
   end
 end
 
